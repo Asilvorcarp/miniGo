e52c03f1cc86cfea32e23acfc543f91c77028e1e
Asilvorcarp <asilvorcarp@qq.com>
2023-05-09 02:55:12 +0800

    fix

--- debug/make.go
+++ debug/make.go
@@ -1,9 +1,9 @@
 package main
 
 func main() {
-	arr := make([][]int, 2, 2)
+	arr := make([][]int, 2)
 	arr[0] = make([]int, 2)
 	arr[1] = make([]int, 2)
-	arr[1][1] = 110
-	putint(arr[1][1])
+	arr[1][1] = 'A'
+	putchar(arr[1][1])
 }
